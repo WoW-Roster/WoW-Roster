f65641573a334228d01af32beea4fb316a38b3ae
JaCait <60408706+JaCait@users.noreply.github.com>
2022-11-23 12:41:12 +0100

    Update views.py

--- roster/views.py
+++ roster/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-def Bosses(request):
+def bosses(request):
     bossesFile = open('./bosses.txt', 'r')
     data = bossesFile.read()
     list = data.split('\n')
     print(list)
-    return render(request, 'Bosses.html', {'list': list})
+    return render(request, 'bosses.html', {'list': list})
