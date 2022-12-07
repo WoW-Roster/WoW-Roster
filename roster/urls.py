9a82f7041fe0ad0dcb9b44689fb654d638cc132e
RioluM <damian04121@gmail.com>
2022-12-07 02:15:44 +0100

    Forms render on one page

--- roster/urls.py
+++ roster/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('players_selection', views.players_selection),
     path('players_selection', views.BossListView.as_view(), name="boss_list_view"),
-    path('boss_update/<int:pk>/', views.BossUpdateView.as_view()),
+    path('boss_update/<int:pk>/', views.BossUpdateView.as_view(), name="boss_update"),
 ]
