5912275947cb33fd94dc6d5f671b7ee526bb34ab
JaCait <filip.tru.26@gmail.com>
2022-12-09 20:31:56 +0100

    Raid buffs

--- characters/models.py
+++ characters/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from characters.types import RoleTypes, ClassTypes
 
 # Create your models here.
 
 class Character(models.Model):
+    ROLES = RoleTypes()
+    CLASSES = ClassTypes()
+
     name = models.CharField(max_length=24)
     game_class = models.CharField(max_length=20)
     role = models.CharField(max_length=6)
@@ -10,3 +14,4 @@ class Character(models.Model):
     server = models.CharField(max_length=2)
     def __str__(self) -> str:
         return f"{self.name}-{self.role}"
+
--- roster/views.py
+++ roster/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count, Q
 from django.views.generic.edit import UpdateView
 from roster.forms import BossRosterForm
 from django.urls import reverse
+from characters.models import Character
 
 
 class BossListView(ListView):
@@ -14,9 +15,16 @@ class BossListView(ListView):
         queryset = super().get_queryset(*args, **kwargs)
         queryset = queryset.annotate(
             players_count=Count("players"),
-            tank_count=Count("players", filter=Q(players__role="tank")),
-            healer_count=Count("players", filter=Q(players__role="healer")),
-            dps_count=Count("players", filter=Q(players__role="dps")),
+            tank_count=Count("players", filter=Q(players__role=Character.ROLES.TANK)),
+            healer_count=Count("players", filter=Q(players__role=Character.ROLES.HEALER)),
+            dps_count=Count("players", filter=Q(players__role=Character.ROLES.DPS)),
+            bloodlust_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.MAGE) | Q(players__game_class=Character.CLASSES.HUNTER) | Q(players__game_class=Character.CLASSES.SHAMAN) | Q(players__game_class=Character.CLASSES.EVOKER)),
+            attack_power_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.WARRIOR)),
+            intellect_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.MAGE)),
+            stamina_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.PRIEST)),
+            magic_damage_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.DEMON_HUNTER)),
+            physical_damage_buff=Count("players", filter=Q(players__game_class=Character.CLASSES.MONK)),
+            devo_aura=Count("players", filter=Q(players__game_class=Character.CLASSES.PALADIN)),
         )
         return queryset
 
